d657d18955e28303a69cf112760b666ae9846c47
Jentz <jentz@allmeans.online>
2023-11-12 21:38:54 +0800

    Creating ChordDiagram

--- visitor2.js
+++ visitor2.js
@@ -23,10 +23,97 @@ function updateCharts(selectedYear, chartType) {
             }
 
         });
-    } else {
+    } else if (chartType === "chord") {
+        d3.csv("data/visitor_region.csv").then(function (data) {
+            // Filter data for the selected year
+            var yearData = data.filter(item => item.Year == selectedYear);
+
+            // Group the data by regions and sum the values
+            var groupedData = groupDataByRegion(yearData);
+
+            // Create a chord chart using the grouped data
+            createChordChart(groupedData);
+
+            console.log(groupedData)
+        });
+    }
+    else {
         console.log("No found")
     }
 }
+
+// Function to group data by regions and sum the values
+function groupDataByRegion(data) {
+
+    var groupedData = {
+        "Eastern Asia": 0,
+        "Southern Asia": 0,
+        "Southeastern Asia": 0,
+        Arabs: 0,
+        Europe: 0,
+        Americas: 0,
+        Oceania: 0,
+        "Latin America": 0,
+        Malaysia: 0,
+        Others: 0,
+        Sarawak: 0, // Initialize Sarawak with 0 value
+    };
+    
+    // Calculate the total value of all regions
+    var total = 0
+
+    // Calculate the remaining values for other regions
+    data.forEach(d => {
+        groupedData["Eastern Asia"] += +d.China + +d.Japan + +d.Taiwan + +d["Hong Kong"] + +d["South Korea"];
+        groupedData["Southern Asia"] += +d["Sri Lanka"] + +d.Bangladesh + +d.Pakistan + +d.India;
+        groupedData["Southeastern Asia"] += +d.Singapore + +d.Brunei + +d.Philippines + +d.Thailand + +d.Indonesia;
+        groupedData.Arabs += +d.Arabs;
+        groupedData.Europe += +d["United Kingdom"] + +d.Germany + +d.France + +d["Nor/Swe/Den/Fin"] + +d["Belg/Lux/Net"] + +d.Russia + +d["Others Europe"];
+        groupedData.Americas += +d.Canada + +d.USA;
+        groupedData.Oceania += +d.Australia + +d["New Zealand"];
+        groupedData["Latin America"] += +d["Latin America"];
+        groupedData.Malaysia += +d["Peninsular Malaysia"] + +d.Sabah;
+        groupedData.Others += +d.Others;
+    });
+
+    // Calculate the total value of all regions except Sarawak
+    var total = d3.sum(Object.values(groupedData));
+
+    // Calculate the fixed value for Sarawak (10% of the total)
+    var sarawakValue = 0.1 * total;
+
+    // Add the fixed value to Sarawak
+    groupedData.Sarawak = sarawakValue;
+
+    return groupedData;
+}
+
+function Matrix(data) {
+    var regions = Object.keys(data);
+    var numRegions = regions.length;
+
+    // Calculate the total value of all regions
+    var total = regions.reduce((sum, region) => sum + data[region], 0);
+
+    // Initialize an empty matrix with zeros
+    var matrix = Array.from({ length: numRegions }, () => Array(numRegions).fill(0));
+
+    regions.forEach((region, i) => {
+        matrix[i][i] = data[region]; // Set the value for the current region
+
+        regions.forEach((otherRegion, j) => {
+            if (i !== j) {
+                // Calculate the value for the connection between regions
+                matrix[i][j] = (0.9 * data[region] * data[otherRegion]) / (0.1 * total * (numRegions - 1));
+            }
+        });
+    });
+
+    console.log(matrix)
+
+    return matrix;
+}
+
 
 // Function to create a pie chart
 function createPieChart(yearData) {
@@ -282,7 +369,7 @@ function createBarChart(data) {
             var total = d.data.Total;
             var label = Math.round(d.data[category] * total);
 
-            var tooltipText = category + ": " + label + ", " + (value*100).toFixed(2) + "% (Total: " + total + ")";
+            var tooltipText = category + ": " + label + ", " + (value * 100).toFixed(2) + "% (Total: " + total + ")";
 
             // Display the tooltip and update its position to follow the cursor
             tooltip
@@ -346,11 +433,107 @@ function createBarChart(data) {
     keys.exit().remove();
 }
 
+function createChordChart(data) {
+    // Clear any existing chart
+    d3.select("#chart").html("");
+    d3.select("#legend").html("");
+
+    // Set the background color to white for the chart element
+    d3.select("#chart").style("background-color", null);
+
+    // Get the regions and create the chord matrix
+    var regions = Object.keys(data);
+
+    // Define the width and height of the chord diagram
+    var width = 800;
+    var height = 800;
+
+    // Calculate the total value of all regions except Sarawak
+    var total = d3.sum(Object.values(data));
+
+    // Calculate the fixed value for Sarawak (10% of the total excluding Sarawak)
+    var sarawakValue = 0.1 * total;
+
+    // Create an SVG element for the chord diagram
+    var svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", `translate(${width / 2},${height / 2})`);
+
+    // Create a chord layout
+    var chords = d3.chord()
+        .padAngle(0.05)
+        .sortSubgroups(d3.descending)
+        .sortChords(d3.ascending);
+
+    // Create a color scale for the regions
+    var colorScale = d3.scaleOrdinal()
+        .domain(d3.range(regions.length))
+        .range(d3.schemeCategory10);
+
+    // Create the arcs for the regions
+    var arc = d3.arc()
+        .innerRadius(width / 2 * 0.35)
+        .outerRadius(width / 2 * 0.4);
+
+    // Create the ribbons connecting the regions
+    var ribbon = d3.ribbon()
+        .radius(width / 2 * 0.35);
+
+    // Create a fixed arc for Sarawak
+    var sarawakArc = d3.arc()
+        .innerRadius(width / 2 * 0.35)
+        .outerRadius(width / 2 * 0.4)
+        .startAngle(0)
+        .endAngle((sarawakValue / total) * 2 * Math.PI);
+
+    // Append the fixed Sarawak arc
+    svg.append("path")
+        .attr("class", "arc")
+        .attr("d", sarawakArc)
+        .style("fill", colorScale(0)); // Set a specific color for Sarawak
+
+    // Create groups for the arcs of other regions
+    var group = svg.selectAll(".group")
+        .data(chords(Matrix(data)).groups)
+        .enter().append("g")
+        .attr("class", "group");
+
+    // Create the arcs for other regions
+    group.append("path")
+        .attr("class", "arc")
+        .attr("d", arc)
+        .style("fill", (d) => colorScale(d.index));
+
+    // Create labels for the regions
+    group.append("text")
+        .each(function (d) { d.angle = (d.startAngle + d.endAngle) / 2; })
+        .attr("dy", ".35em")
+        .attr("transform", function (d) {
+            return `rotate(${(d.angle * 180 / Math.PI - 90)}) ` +
+                `translate(${width / 2 * 0.47}) ` +
+                (d.angle > Math.PI ? "rotate(180)" : "");
+        })
+        .style("text-anchor", function (d) { return d.angle > Math.PI ? "end" : null; })
+        .text((d) => regions[d.index]);
+
+    // Create ribbons connecting the regions
+    svg.selectAll(".chord")
+        .data(chords(Matrix(data)).filter((d) => d.source.index !== 0))
+        .enter().append("path")
+        .attr("class", "chord")
+        .attr("d", ribbon)
+        .style("fill", (d) => colorScale(d.source.index))
+        .style("opacity", 0.8);
+}
+
 
 // Declare variables in a scope accessible by all functions
 var yearSlider;
 var sliderTitle;
-var currentChartType = "bar";
+var currentChartType = "chord";
 
 // Function to update the title with the selected year
 function updateSliderTitle() {
@@ -364,6 +547,7 @@ document.addEventListener("DOMContentLoaded", function () {
     yearSlider = document.getElementById("year"); // Define yearSlider in this scope
     var pieChartButton = document.getElementById("pieChartButton");
     var barChartButton = document.getElementById("barChartButton");
+    var ChordChartButton = document.getElementById("ChordChartButton");
     sliderTitle = document.getElementById('sliderTitle');
 
     // Add an event listener to the slider input
@@ -386,6 +570,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // Add event listener for the bar chart button
     barChartButton.addEventListener("click", function () {
         currentChartType = "bar";
+        updateCharts(yearSlider.value, currentChartType);
+    });
+
+    ChordChartButton.addEventListener("click", function () {
+        currentChartType = "chord";
         updateCharts(yearSlider.value, currentChartType);
     });
 });
